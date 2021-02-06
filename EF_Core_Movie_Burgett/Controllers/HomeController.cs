99be4340622f2decf6941b2367c13942492080fc
Chadwick Burgett <54729703+chadwickburgett@users.noreply.github.com>
2021-02-06 10:54:07 -0600

    EF Core Movie 1.3
    Updating project files

---
+++ EF_Core_Movie_Burgett/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EF_Core_Movie_Burgett.Models;
+
+namespace EF_Core_Movie_Burgett.Controllers
+{
+    public class HomeController : Controller
+    {
+        private MovieContext context { get; set; }
+
+        public HomeController(MovieContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IActionResult Index()
+        {
+            var movies = context.Movies
+                .Include(m => m.Genre)
+                .OrderBy(m => m.Name)
+                .ToList();
+            return View(movies);
+        }
+    }
+}
---
+++ EF_Core_Movie_Burgett/Models/Genre.cs
@@ -0,0 +1,8 @@
+﻿namespace EF_Core_Movie_Burgett.Models
+{
+    public class Genre
+    {
+        public string GenreId { get; set; }
+        public string Name { get; set; }
+    }
+}
