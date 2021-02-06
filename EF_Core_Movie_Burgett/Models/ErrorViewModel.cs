178ec4d99bb8466795cfde444a75d93494dd34b1
Chadwick Burgett <54729703+chadwickburgett@users.noreply.github.com>
2021-02-06 09:04:21 -0600

    EF Core Movie Project
    This is the project for chapter 4 in the textbook

---
+++ EF_Core_Movie_Burgett/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EF_Core_Movie_Burgett.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
