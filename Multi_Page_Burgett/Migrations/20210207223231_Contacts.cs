8cee918df5fcf7144987bcf3cfcb05d8b59c3521
Chadwick Burgett <54729703+chadwickburgett@users.noreply.github.com>
2021-02-12 21:24:24 -0600

    update
    update

---
+++ Multi_Page_Burgett/Migrations/20210207223231_Contacts.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Multi_Page_Burgett.Migrations
+{
+    public partial class Contacts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- Multi_Page_Burgett/obj/Debug/netcoreapp3.1/Multi_Page_Burgett.AssemblyInfo.cs
+++ Multi_Page_Burgett/obj/Debug/netcoreapp3.1/Multi_Page_Burgett.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("d7965c47-3ff6-4001-b1fe-78a0848d5329")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Multi_Page_Burgett")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
