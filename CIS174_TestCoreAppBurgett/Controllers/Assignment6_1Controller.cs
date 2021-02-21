baaa349be4e123f39dcc62aaab5029c7bbe0e403
Chadwick Burgett <54729703+chadwickburgett@users.noreply.github.com>
2021-02-20 21:26:59 -0600

    Update
    Update

---
+++ CIS174_TestCoreAppBurgett/Controllers/Assignment6_1Controller.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CIS174_TestCoreAppBurgett.Controllers
+{
+    public class Assignment6_1Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("[action]/{id:range(1,10)}")]
+        public IActionResult Assignment6_1(int id = 1)
+        {
+            return View(id);
+        }
+    }
+}
---
+++ CIS174_TestCoreAppBurgett/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CIS174_TestCoreAppBurgett.Models
+{
+    public class Student
+    {
+        public string F_Name { get; set; }
+        public string L_Name { get; set; }
+        public string Grade { get; set; }
+    }
+}
