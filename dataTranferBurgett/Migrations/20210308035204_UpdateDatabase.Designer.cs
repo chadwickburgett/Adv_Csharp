4702886b39ceaf4fccebcce4fe0bf403497cbd52
Chadwick Burgett <54729703+chadwickburgett@users.noreply.github.com>
2021-03-07 21:52:51 -0600

    update
    update

--- dataTranferBurgett/Migrations/20210308035204_UpdateDatabase.Designer.cs
+++ dataTranferBurgett/Migrations/20210308035204_UpdateDatabase.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using dataTranferBurgett.Models;
 
 namespace dataTranferBurgett.Migrations
 {
     [DbContext(typeof(CountryContext))]
-    partial class CountryContextModelSnapshot : ModelSnapshot
+    [Migration("20210308035204_UpdateDatabase")]
+    partial class UpdateDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ dataTranferBurgett/Migrations/20210308035204_UpdateDatabase.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace dataTranferBurgett.Migrations
+{
+    public partial class UpdateDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "CountryID",
+                keyValue: "Cn",
+                column: "FlagImage",
+                value: "canada.jfif");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "CountryID",
+                keyValue: "Mx",
+                column: "FlagImage",
+                value: "mexico.jfif");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "CountryID",
+                keyValue: "Cn",
+                column: "FlagImage",
+                value: "canada.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Country",
+                keyColumn: "CountryID",
+                keyValue: "Mx",
+                column: "FlagImage",
+                value: "Mexico.jfif");
+        }
+    }
+}
