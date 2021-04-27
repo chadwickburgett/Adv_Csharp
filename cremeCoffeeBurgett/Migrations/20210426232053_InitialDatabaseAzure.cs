7bd2d89d557e7e9a69eee6d6acacd2fb7ef4c393
Chadwick Burgett <54729703+chadwickburgett@users.noreply.github.com>
2021-04-26 19:11:49 -0500

    update
    update

---
+++ cremeCoffeeBurgett/Migrations/20210426232053_InitialDatabaseAzure.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace cremeCoffeeBurgett.Migrations
+{
+    public partial class InitialDatabaseAzure : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
